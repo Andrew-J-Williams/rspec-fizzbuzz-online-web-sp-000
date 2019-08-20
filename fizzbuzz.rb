7003472c8a90b2f178377017933a511a714ae543
Andrew Williams <1williamsaj@gmail.com>
2019-08-20 23:52:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@
 def fizzbuzz(number) #First, we define our method and include an argument.
   if (number % 3 == 0) && (number % 5 == 0) #Second, we want to make our main condition all encompassing. Thus we test for both first.
     return "FizzBuzz" #We use return to avoid the 'nil' that comes with using 'puts'.
-  elsif number % 3 == 0
+  elsif number % 3 == 0 #Third, move on to just one case.
     return "Fizz"
   elsif number % 5 == 0
     return "Buzz"
